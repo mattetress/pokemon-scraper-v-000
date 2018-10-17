4ff55e9b28bd4d103e9205d130c88a2de19cdb18
Matt Etress <mattetress@me.com>
2018-10-17 09:40:33 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,8 +2,9 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
   def initialize(id, name, type, db)
-    @id = id 
-    @name = name 
+    @id = id
+    @name = name
     @type = type
     @db = db
+  end
 end
